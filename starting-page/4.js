2eef829a774870b137dafaea37a7901030eb5b4a
madlock <hassi3914@gmail.com>
2025-08-27 09:36:17 +0500

    added css animation

---
+++ starting-page/4.js
@@ -0,0 +1,15 @@
+import Data from "./1.json" 
+
+/*
+async function printData(path) {
+
+    const DataJson = await fetch(path);
+    const Data = DataJson.json();
+    console.log(Data.name);
+
+}
+
+
+printData("1.json");
+*/
+console.log(Data.name)
